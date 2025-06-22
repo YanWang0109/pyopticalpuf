8fe6b154e328e286430b4fa25855ea553cc51d09
hallidab <halliday_blake@outlook.com>
2025-06-22 15:14:33 +0100

    Implemented overtime testing example

--- Examples/4 - Time series testing.py
+++ Examples/4 - Time series testing.py
@@ -1,17 +1,19 @@
 import sys
 import os
-OPUF_DIR = "Refactor\OPUFToolkit"
+from typing import List
+
+from matplotlib import pyplot as plt
+import numpy as np
+OPUF_DIR = ""
 sys.path.append(os.path.dirname(OPUF_DIR))
 
 from pyOpticalPUF.Utility import ImageHelper
 from itertools import product
 from pyOpticalPUF.HammingDistanceCalculators import HammingDistanceCalculator
 from pyOpticalPUF.Distributions import GuassianDistribution
+from pyOpticalPUF.Display import DistributionDisplay
 
-if __name__ == "__main__":
-    intraFingerprints = ImageHelper.loadImagesFromFolder("Refactor\example output\intras")
-    interFingerprints = ImageHelper.loadImagesFromFolder("Refactor\example output\inters")
-
+def calculateHammingDistance(intraFingerprints: List[np.ndarray], interFingerprints: List[np.ndarray]):
     #1. Create every combination of intras (ignoring comparing the same image twice) and inters 
     allIntraCombinations = [(intraOne, intraTwo) for intraOne, intraTwo in product(intraFingerprints, intraFingerprints) if intraOne is not intraTwo]
     allIntraInterCombinations = product(intraFingerprints, interFingerprints)
@@ -19,8 +21,33 @@ if __name__ == "__main__":
     #2. Calculate hamming distance
     intraHammingDistances = [HammingDistanceCalculator.calculateHammingDistance(intraOne, intraTwo).hammingDistance for intraOne, intraTwo in allIntraCombinations]
     intersHammingDistances = [HammingDistanceCalculator.calculateHammingDistance(intra, inter).hammingDistance for intra, inter in allIntraInterCombinations]
+    return intraHammingDistances, intersHammingDistances
 
-    #3. Fit Guassian distribution and calculate metrics
-    intraGuassian = GuassianDistribution.fromData(intraHammingDistances)
-    interGuassian = GuassianDistribution.fromData(intersHammingDistances)
+if __name__ == "__main__":
 
+    intraInterFolders = [
+        ["example output\intras", "example output\inters"],
+        ["example output\intras", "example output\inters"],
+        ["example output\intras", "example output\inters"],
+    ]
+
+    intraHammingDistances = []
+    interHammingDistances = []
+
+    for intraFolder, interFolder in intraInterFolders:
+        intraFingerprints = ImageHelper.loadImagesFromFolder(intraFolder)
+        interFingerprints = ImageHelper.loadImagesFromFolder(interFolder)
+        intraHDs, interHDs = calculateHammingDistance(intraFingerprints, interFingerprints)
+        
+        intraHammingDistances.append(intraHDs)
+        interHammingDistances.append(interHDs) 
+
+    intraGuassians = [GuassianDistribution.fromData(dist) for dist in intraHammingDistances]
+    interGuassians = [GuassianDistribution.fromData(dist) for dist in interHammingDistances]
+
+    xRange = np.arange(0, 1, 0.01)
+    for i, (intra, inter) in enumerate(zip(intraGuassians, interGuassians)):
+        DistributionDisplay.plot3D(xRange, np.repeat(i, xRange.shape[0]), *intraGuassians, *interGuassians)
+    plt.xlabel("Hamming distance")
+    plt.ylabel("Time")
+    plt.show()
