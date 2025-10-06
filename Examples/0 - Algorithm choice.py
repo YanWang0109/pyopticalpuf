3898cf7ee95f271a6b6241808b8871a037cb6009
YanWang0109 <wang.yan@kaust.edu.sa>
2025-10-06 13:21:43 +0000

    update code from colab

--- Examples/0 - Algorithm choice.py
+++ Examples/0 - Algorithm choice.py
@@ -4,6 +4,8 @@ import os
 import cv2
 OPUF_DIR = ""
 sys.path.append(os.path.dirname(OPUF_DIR))
+import matplotlib.pyplot as plt
+
 
 from pyOpticalPUF.Fingerprinting.AHB import *
 from pyOpticalPUF.Fingerprinting.LBP import *
@@ -14,26 +16,33 @@ if __name__ == "__main__":
     while True:
         pathToImage = input("Enter path to image:")
         pathToImage = Path(pathToImage)
-        print("No image found.")
         if pathToImage.exists():
             break
+        print("No image found.")
     
     image = cv2.imread(pathToImage, cv2.IMREAD_GRAYSCALE)
 
-    lbpParameters = LBPParameters(50, 24, (100, 100))
-    lbpFingerprint = LBP.calculateFingerprint(image, lbpParameters)
+    # lbpParameters = LBPParameters(50, 24, (100, 100))
+    # lbpFingerprint = LBP.calculateFingerprint(image, lbpParameters)
     
     sauvolaParameters = SauvolaParameters((100,100), (5,5), 0.005)
     sauvolaFingerprint = Sauvola.calculateFingerprint(image, sauvolaParameters) 
 
-    ahbParameters = AHBParameters((100, 100), kernelSize=35)
-    ahbFingerprint = AHB.calculateFingerprint(image, ahbParameters)
+    # ahbParameters = AHBParameters((100, 100), kernelSize=35)
+    # ahbFingerprint = AHB.calculateFingerprint(image, ahbParameters)
 
-    otsuParameters = OtsuParameters((100, 100))
-    otsuFingerprint = Ostu.calculateFingerprint(image, otsuParameters)
+    # otsuParameters = OtsuParameters((100, 100))
+    # otsuFingerprint = Ostu.calculateFingerprint(image, otsuParameters)
 
-    cv2.imshow("LBP Fingerprint", lbpFingerprint.astype(np.float32))
-    cv2.imshow("Sauvola Fingerprint", sauvolaFingerprint.astype(np.float32))
-    cv2.imshow("AHB Fingerprint", ahbFingerprint.astype(np.float32))
-    cv2.imshow("Otsu Fingerprint", otsuFingerprint.astype(np.float32))
-    cv2.waitKey(0)
+    plt.figure(figsize=(5,5))
+    plt.imshow(sauvolaFingerprint, cmap='gray', interpolation='nearest')
+    plt.title('Sauvola')
+    plt.axis('off')            # 可选：去坐标轴
+    plt.tight_layout()
+    plt.show()
+
+    # cv2.imshow("LBP Fingerprint", lbpFingerprint.astype(np.float32))
+    # cv2.imshow("Sauvola Fingerprint", sauvolaFingerprint.astype(np.float32))
+    # cv2.imshow("AHB Fingerprint", ahbFingerprint.astype(np.float32))
+    # cv2.imshow("Otsu Fingerprint", otsuFingerprint.astype(np.float32))
+    # cv2.waitKey(0)
